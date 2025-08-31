ba918225df60001d341dbba2c62ec3f17e0a3f5a
MosharrafXGit <memosharraf@gmail.com>
2025-08-31 16:54:34 +0530

    Initial

---
+++ src/Pattern/SevenTeen.java
@@ -0,0 +1,48 @@
+package Pattern;
+
+public class SevenTeen {
+
+	static void print(int n)
+    {
+        
+        int num=0;
+        for(int i=0;i<n;i++)
+        {
+            //space
+            for(int j=0;j<n-i-1;j++)
+            {
+                System.out.print(" ");
+            }
+            
+            //print
+            char ch='A';
+            int bp = (2*i+1)/2;
+            for(int j=1;j<=2*i+1;j++)
+            {
+                System.out.print(ch);
+                if(j<=bp)
+                {
+                    ch++;
+                }
+                else
+                {
+                    ch--;
+                }
+                    
+            }    
+            //space
+            for(int j=0;j<n-i-1;j++)
+            {
+                System.out.print(" ");
+            }
+            
+            System.out.println();
+        }
+    }
+    
+    public static void main(String[] args) {
+        
+        SevenTeen.print(5);
+
+    }
+}
